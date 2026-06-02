8814365227ab8debe4d89763b3cf6acb59ed8208
mudit saraogi <muditsaraogi21@gmail.com>
2026-06-02 20:42:50 +0530

    Add additional alert messages on button click

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ const button = document.querySelector(".btn");
 
 button.addEventListener("click", function () {
   alert("Successfully removed block button");
+  alert("you happy now???");
+  alert("yeah love you too");
 });
